f3ff33a4004972317e569ddefd9ca8e82ceb0d8e
jaroslav <jazzoslav@gmail.com>
2016-06-16 16:55:20 +0200

    elogr(): support Slim request.

--- src/elog.php
+++ src/elog.php
@@ -29,8 +29,14 @@ if (!function_exists('elogr')) {
 		$last = (object) $db[0];
 		if (isset($last->args)) {
 			foreach ($last->args as $i => $a) {
+
+				if (class_exists('\Slim\Http\Request') and ($a instanceof \Slim\Http\Request)) {
+					elog( [@$last->class . '@' . @$last->function => $a->getParams() ] );
+					break;
+				}
+				
 				if (class_exists('\Illuminate\Http\Request') and ($a instanceof \Illuminate\Http\Request)) {
-					elog( [@$last->class . '@' . $last->function => $a->all() ] );
+					elog( [@$last->class . '@' . @$last->function => $a->all() ] );
 					break;
 				}
 			}
@@ -43,8 +49,8 @@ if (!function_exists('elogm')) {
 	function elogm($data) {
 		$db = debug_backtrace();
 		array_shift($db);
-		$last = (object) $db[0];
-		$caller = @$last->class . '@' . $last->function;
+		$last = (object) @$db[0];
+		$caller = @$last->class . '@' . @$last->function;
 		elog([$caller => $data]);
 	}
 }
